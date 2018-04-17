b29f4eb3e93343b1811e637df09acfd81b47a5d0
timorss <timorss@gmail.com>
2018-04-17 23:31:39 +0300

    push

--- server/server.js
+++ server/server.js
@@ -37,21 +37,21 @@ app.get('/todos', (req, res) => {
   })
 })
 
-// app.get('/todos/:id', (req, res) => {
-//   var id = req.params.id
-//   if (!ObjectID.isValid(id)) {
-//     return res.status(404).send()
-//   }
-//   Todo.findById(id).then((todo) => {
-//     if (!todo) {
-//       res.status(404).send()
-//     }
-//     res.send({ todo })
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send()
+    }
+    res.send({ todo })
 
-//   }).catch((err) => {
-//     res.status(400).send()
-//   })
-// })
+  }).catch((err) => {
+    res.status(400).send()
+  })
+})
 
 app.listen(port, () => {
   console.log(`started om port ${port}`);
